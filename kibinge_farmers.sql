7023f465643b2e55641f3ee35f0b127af6cdda8d
Agume_kenneth <180535228+Agumekenneth@users.noreply.github.com>
2025-03-20 10:56:03 +0300

    practice

--- kibinge_farmers.sql
+++ kibinge_farmers.sql
@@ -27,3 +27,30 @@ INSERT INTO crop(cropID,cropName,type,season ) VALUES(102,'maize','food crop','d
 INSERT INTO crop(cropID,cropName,type,season ) VALUES(103,'beans','food crop','rainy');
 
 SELECT * FROM crop
+
+CREATE TABLE farmer_crop(
+    farmerCropID INT(10) PRIMARY KEY,
+    farmerID INT(10),
+    cropID INT(10) ,
+    areaCultivated VARCHAR(255),
+    yeild VARCHAR(255),
+    FOREIGN KEY (farmerID) REFERENCES farmers(farmerID),
+    FOREIGN KEY (cropID) REFERENCES crop(cropID)
+);
+
+
+INSERT INTO farmer_crop(farmerCropID,farmerID,cropID,areaCultivated,yeild) VALUES(1,1,101,5,100);
+INSERT INTO farmer_crop(farmerCropID,farmerID,cropID,areaCultivated,yeild) VALUES(2,1,102,3.5,700);
+INSERT INTO farmer_crop(farmerCropID,farmerID,cropID,areaCultivated,yeild) VALUES(3,3,103,2,400);
+SELECT*FROM farmer_crop;
+
+CREATE TABLE market(
+    marketID INT(10) PRIMARY KEY,
+    marketName VARCHAR(255),
+    location VARCHAR(255)
+);
+
+INSERT INTO market(marketID,marketName,location) VALUES(201,'kibinge market','kibinge');
+INSERT INTO market(marketID,marketName,location) VALUES(202,'masaka central market','masaka');
+INSERT INTO market(marketID,marketName,location) VALUES(203,'lwengo farmers market','lwengo');
+SELECT* FROM market
